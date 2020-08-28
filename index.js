6e2ce2705e17064ac1d09ee4379c54518dabb54e
Evan Monroy <evanmonroy@gmail.com>
2020-08-28 13:28:19 -0400

    Basic Routes Implemented

--- index.js
+++ index.js
@@ -8,19 +8,175 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
-//ROUTES//
+//ROUTES//USERS//
 
-//create a todo
+//create a user
+app.post("/users", async(req, res) => 
+{
+    try
+    {
+        const { username } = req.body;
+        const newUser = await pool.query(
+            "INSERT INTO users (username) VALUES ($1) RETURNING *",
+            [username]
+        );
 
-//get all todos
-
-//get a todo
-
-//update a todo
-
-//delete a todo
-
-app.listen(5000, () => {
-    console.log("server has started on port 5000");
+        res.json(newUser.rows[0]);
+    }
+    catch (err)
+    {
+        console.error(err.message);
+    }
 });
 
+//get all users
+app.get("/users", async(req, res) => 
+{
+    try
+    {
+        const allUsers = await pool.query("SELECT * FROM users");
+        res.json(allUsers.rows);
+    }
+    catch (err)
+    {
+        console.error(err.message);
+    }
+});
+
+//get a user SEARCHES by ID
+app.get("/users/:id", async(req, res) =>
+{
+    try
+    {
+        const { id } = req.params;
+        const user = await pool.query("SELECT * FROM users WHERE user_id = $1", [id]);
+
+        res.json(user.rows[0]);
+    }
+    catch (err)
+    {
+        console.error(err.message);
+    }
+});
+
+//update a user SEARCHES by ID
+app.put("/users/:id", async(req, res) =>
+{
+    try 
+    {
+        const { id } =  req.params;
+        const { username } = req.body;
+        const updateUser = await pool.query("UPDATE users SET username = $1 WHERE user_id = $2", [username, id]);
+
+        res.json("User was updated!");
+    } 
+    catch (err) 
+    {
+        console.error(err.message);
+    }
+});
+
+//delete a U S E R SEARCHES by ID
+app.delete("/users/:id", async(req, res) =>
+{
+    try 
+    {
+        const { id } = req.params;
+        const deleteUser = await pool.query("DELETE FROM users WHERE user_id = $1", [id]);
+
+        res.json("User was D E L E T E D");
+    } 
+    catch (err) 
+    {
+        console.error(err.message);
+    }
+});
+
+//ROUTES//MOUNTS//
+
+//create a mount
+app.post("/mounts", async(req, res) => 
+{
+    try
+    {
+        const { mount_name } = req.body;
+        const newMount = await pool.query(
+            "INSERT INTO mounts (mount_name) VALUES ($1) RETURNING *",
+            [mount_name]
+        );
+
+        res.json(newMount.rows[0]);
+    }
+    catch (err)
+    {
+        console.error(err.message);
+    }
+});
+
+//get all mounts
+app.get("/mounts", async(req, res) => 
+{
+    try
+    {
+        const allMounts = await pool.query("SELECT * FROM mounts");
+        res.json(allMounts.rows);
+    }
+    catch (err)
+    {
+        console.error(err.message);
+    }
+});
+
+//get a mount SEARCHES by ID
+app.get("/mounts/:id", async(req, res) =>
+{
+    try
+    {
+        const { id } = req.params;
+        const mount = await pool.query("SELECT * FROM mounts WHERE mount_id = $1", [id]);
+
+        res.json(mount.rows[0]);
+    }
+    catch (err)
+    {
+        console.error(err.message);
+    }
+});
+
+//update a mount SEARCHES by ID
+app.put("/mounts/:id", async(req, res) =>
+{
+    try 
+    {
+        const { id } =  req.params;
+        const { mount_name } = req.body;
+        const updateMount = await pool.query("UPDATE mounts SET mount_name = $1 WHERE mount_id = $2", [mount_name, id]);
+
+        res.json("Mount was updated!");
+    } 
+    catch (err) 
+    {
+        console.error(err.message);
+    }
+});
+
+//delete a mount SEARCHES by ID
+app.delete("/mounts/:id", async(req, res) =>
+{
+    try 
+    {
+        const { id } = req.params;
+        const deleteMount = await pool.query("DELETE FROM mounts WHERE mount_id = $1", [id]);
+
+        res.json("Mount was D E L E T E D");
+    } 
+    catch (err) 
+    {
+        console.error(err.message);
+    }
+});
+
+app.listen(5000, () => 
+{
+    console.log("server has started on port 5000");
+});
