553cb13d3b18cce38eec5ec544ffc6b1831669cb
Sofia Lee <sofia819@gmail.com>
2021-04-03 21:40:43 -0400

    ssl

--- data-access/db.js
+++ data-access/db.js
@@ -2,11 +2,9 @@ const pgp = require('pg-promise')();
 
 const config = {
   connectionString: process.env.DATABASE_URL,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
   },
 };
 const pool = pgp(config);
