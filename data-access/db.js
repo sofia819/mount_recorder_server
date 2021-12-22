be744714d8952f2d5e9267d776bafc983261335a
Sofia Lee <sofia819@gmail.com>
2021-12-22 17:46:25 -0500

    Revert "Add SSL rule for local"

--- data-access/db.js
+++ data-access/db.js
@@ -1,13 +1,11 @@
 const pgp = require('pg-promise')();
 
-let ssl = null;
-if (process.env.NODE_ENV === 'development') {
-  ssl = { rejectUnauthorized: false };
-}
-
 const config = {
   connectionString: process.env.DATABASE_URL,
-  ssl: ssl,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 };
 const pool = pgp(config);
 
